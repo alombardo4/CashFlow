01910f600e7716e5bc52b875e4e35ca50206feac
alombardo4 <alombardo7@gatech.edu>
2014-01-25 13:59:15 -0500

    Created Welcome Activity and set it up with a button
    Button has a onClick method that needs to be filled in once the Login
    activity is created

--- CashFlow/gen/com/mikesandfriends/cashflow/R.java
+++ CashFlow/gen/com/mikesandfriends/cashflow/R.java
@@ -24,7 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int iconview=0x7f080001;
+        public static final int loginbutton=0x7f080002;
+        public static final int welcometext=0x7f080000;
     }
     public static final class layout {
         public static final int activity_welcome=0x7f030000;
@@ -35,7 +37,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int appicon=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int login=0x7f050003;
+        public static final int welcometext=0x7f050004;
     }
     public static final class style {
         /** 
--- CashFlow/src/com/mikesandfriends/cashflow/WelcomeActivity.java
+++ CashFlow/src/com/mikesandfriends/cashflow/WelcomeActivity.java
@@ -2,14 +2,43 @@ package com.mikesandfriends.cashflow;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Menu;
-
+import android.view.View;
+import android.widget.Button;
+/**
+ * This is the Welcome screen that is displayed as soon as the
+ * user opens the app
+ * @author Alec Lombardo
+ * @verison 1.0
+ */
 public class WelcomeActivity extends Activity {
 
-    @Override
+	
+	@Override
+	/**
+	 * Called when the view is created
+	 */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        
+        getActionBar().setTitle("Welcome to CashFlow");
+        getActionBar().setBackgroundDrawable(new ColorDrawable(
+        		Color.parseColor("#035986")));
+        
+        Button loginButton = (Button) findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+        	/**
+        	 * Called when the user clicks on the button
+        	 */
+			@Override
+			public void onClick(View v) {
+				// TODO put in the transition to login page
+				
+			}
+		});
     }
 
 
